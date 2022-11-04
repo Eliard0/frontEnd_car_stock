6319e71fa42413ba49eb1f0dfea7ce1751c6aa34
Eliardo <57414074+Eliard0@users.noreply.github.com>
2022-11-04 16:02:39 -0300

    responsividade ok, tela de ver carros cadastrados com dados que veem do back end ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Cadastro from './pages/cadastro';
 import ListCarro from './pages/listCarro';
 
 function App() {
+
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Cadastro/>}/>
-          <Route path="/lista" element={<ListCarro/>}/>
+          <Route path="/" element={<Cadastro />} />
+          <Route path="/lista" element={<ListCarro/>}
+            // typeof listCarros !== "undefined" &&
+            // listCarros.map((value) => {
+            //   return <ListCarro
+            //     key={value.id}
+            //     id={value.id}
+            //     marca={value.marca}
+            //     modelo={value.modelo}
+            //     cor={value.cor}
+            //     ano={value.ano_fabricacao}
+            //     ano_modelo={value.ano_modelo}
+            //     cambio={value.tipo_cambio}
+            //   />
+            // })}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/listCarro.js
+++ src/pages/listCarro.js
@@ -1,13 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
 
-const listCarro = () => {
+export default function ListCarro() {
+
+  const [listCarros, setListCarros] = useState();
+
+  useEffect(() => {
+    Axios.get("http://localhost:3003/getCarros").then((response) => {
+      setListCarros(response.data)
+    })
+  }, []);
+
+  const dados = typeof listCarros !== "undefined" &&
+    listCarros.map((c, i) => {
+      return <tr>
+        <td key={i.id}> {c.id} </td>
+        <td> {c.marca} </td>
+        <td> {c.modelo} </td>
+        <td> {c.cor} </td>
+        <td> {c.ano_fabricacao} </td>
+        <td> {c.ano_modelo} </td>
+        <td> {c.tipo_cambio} </td>
+      </tr>
+    });
+
+  console.log(listCarros);
   return (
-    <div>
-      <h1>
-        listCarro
-      </h1>
+    <div className='ml-5 mr-5 mt-5'>
+      <h2>Lista de Carros</h2>
+      <label>
+        <span>Tipo de Cambio:</span>
+        <select className="form-control" name="cambio">
+          <option>Selecione</option>
+          <option value="manual">Manual</option>
+          <option value="automatico">Automático</option>
+        </select>
+      </label>
+      <table className="table ">
+        <thead>
+          <tr>
+            <th scope="col">Nº</th>
+            <th scope="col">Marca</th>
+            <th scope="col">Modelo</th>
+            <th scope="col">Cor</th>
+            <th scope="col">Ano de fabricação</th>
+            <th scope="col">Ano do modelo</th>
+            <th scope="col">tipo de cambio</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dados}
+        </tbody>
+      </table>
     </div>
   )
 }
-
-export default listCarro
